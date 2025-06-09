5ad89afb2f205a8b5c79a189f623a4929c7bd790
percivalmacalintal <percival_macalintal@dlsu.edu.ph>
2025-06-09 23:08:12 +0800

    implemented initial info layout

--- app/src/main/java/ph/edu/dlsu/ccs/mobicom/remedication/MainActivity.kt
+++ app/src/main/java/ph/edu/dlsu/ccs/mobicom/remedication/MainActivity.kt
@@ -2,46 +2,10 @@ package ph.edu.dlsu.ccs.mobicom.remedication
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ph.edu.dlsu.ccs.mobicom.remedication.ui.theme.ReMedicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ReMedicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ReMedicationTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_info)
     }
 }
