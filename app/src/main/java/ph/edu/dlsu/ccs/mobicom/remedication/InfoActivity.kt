021a2b75a3555d17be93de53412e43459e52fdaa
Percival Macalintal <percival_macalintal@dlsu.edu.ph>
2025-06-10 19:00:01 +0800

    Renamed MainActivity to InfoActivity for merge

--- app/src/main/java/ph/edu/dlsu/ccs/mobicom/remedication/InfoActivity.kt
+++ app/src/main/java/ph/edu/dlsu/ccs/mobicom/remedication/InfoActivity.kt
@@ -3,7 +3,7 @@ package ph.edu.dlsu.ccs.mobicom.remedication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 
-class MainActivity : ComponentActivity() {
+class InfoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
