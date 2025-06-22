0757cf3b796422e27c75e246e4f067eca6078eb1
Arjay Kyle Rabe <arjay_rabe@dlsu.edu.ph>
2025-06-22 19:38:07 +0800

    - used medicine generator in section data generator instead of checklist data generator - deleted checklist data generator

--- app/src/main/java/ph/edu/dlsu/ccs/mobicom/remedication/SectionDataGenerator.kt
+++ app/src/main/java/ph/edu/dlsu/ccs/mobicom/remedication/SectionDataGenerator.kt
@@ -1,14 +1,25 @@
 package ph.edu.dlsu.ccs.mobicom.remedication
 
 class SectionDataGenerator {
-    companion object{
+    companion object {
         fun generateData(): ArrayList<Section> {
-            var tempList = ArrayList<Section>()
-            tempList.add(Section("After Midnight", ChecklistDataGenerator.generateData()))
-            tempList.add(Section("Morning", ChecklistDataGenerator.generateData()))
-            tempList.add(Section("Afternoon", ChecklistDataGenerator.generateData()))
-            tempList.add(Section("Night", ChecklistDataGenerator.generateData()))
-            return tempList
+            val sections = ArrayList<Section>()
+            val medicineList = MedicineGenerator.generateData()
+
+            val sectionTitles = listOf("After Midnight", "Morning", "Afternoon", "Night")
+
+            for (i in 0..3) {
+                val filteredMedicines = medicineList.filter { it.timeOfDay.contains(i) }
+
+                if (filteredMedicines.isNotEmpty()) {
+                    val checklistItems = filteredMedicines.map {
+                        Checklist(it.imageId, it.name, "${it.dosage} ${it.unit}")
+                    }
+                    sections.add(Section(sectionTitles[i], ArrayList(checklistItems)))
+                }
+            }
+
+            return sections
         }
     }
 }
